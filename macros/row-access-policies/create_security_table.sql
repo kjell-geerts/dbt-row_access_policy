56468d81f5491c817771b845c6b499176b56e158
kjell.geerts@biztory.be <kjell.geerts@biztory.be>
2023-05-02 19:45:00 +0000

    add security table

--- macros/row-access-policies/create_security_table.sql
+++ macros/row-access-policies/create_security_table.sql
@@ -24,6 +24,8 @@
         ('BUSINESS_CATEGORY', 'Burgers', 'wim.kegels@biztory.be', getvariable('VAR_UPDATE_TIME')),
         ('BUSINESS_CATEGORY', 'Vegan', 'michiel.smulders@biztory.be', getvariable('VAR_UPDATE_TIME')),
         ('BUSINESS_CATEGORY', 'ALL VALUES', 'kjell.geerts@biztory.be', getvariable('VAR_UPDATE_TIME'))
+        ('BUSINESS_CATEGORY', 'ALL VALUES', 'artur.lucas@biztory.be', getvariable('VAR_UPDATE_TIME')),
+        ('STATE', 'ALL VALUES', 'artur.lucas@biztory.be', getvariable('VAR_UPDATE_TIME')),
         ;
     {% endif %}
 {% endmacro %}
